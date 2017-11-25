a9f7a0ac46c941127a1813abe6e94fc474cfa8ab
Brendan Almonte <almonteb@datawh.net>
2017-11-25 14:25:29 -0800

    expose token executor

--- executor.go
+++ executor.go
@@ -42,11 +42,11 @@ type AwsRequester interface {
 
 // Create an AWS executor with a specified endpoint and AWS parameters.
 func NewAwsExecutor(endpoint, region, accessKey, secretKey string) *AwsExecutor {
-	return newAwsExecutorToken(endpoint, region, accessKey, secretKey, "")
+	return NewAwsExecutorToken(endpoint, region, accessKey, secretKey, "")
 }
 
 // Create an AWS executor with a specified endpoint and AWS parameters.
-func newAwsExecutorToken(endpoint, region, accessKey, secretKey, sessionToken string) *AwsExecutor {
+func NewAwsExecutorToken(endpoint, region, accessKey, secretKey, sessionToken string) *AwsExecutor {
 	signer := aws.AwsSigner{
 		Region:       region,
 		AccessKey:    accessKey,
